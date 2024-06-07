9dce66931fbcc0249fea44ed36a95355e3360146
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2024-06-07 22:58:56 +0500

    fix(nvim): fix conceal level and misspells in markdown

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,3 +1,5 @@
-vim.opt_local.conceallevel = 1
+vim.opt_local.conceallevel = 2
 vim.opt_local.colorcolumn = "0"
 vim.opt_local.spell = true
+
+vim.api.nvim_set_hl(0, "SpellBad", { underline = true })
