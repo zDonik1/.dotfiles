3a58bd737dc4346ab0659270ef128b4e1ee3370e
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2024-06-18 23:47:56 +0500

    fix(nixos): update timezone to Tashkent

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,16 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-# NixOS-WSL specific options are documented on the NixOS-WSL repository:
-# https://github.com/nix-community/NixOS-WSL
-
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 
 {
   imports = [ ./hardware-configuration.nix ];
@@ -27,6 +15,8 @@
     };
     defaultUserShell = pkgs.nushell;
   };
+
+  time.timeZone = "Asia/Tashkent";
 
   environment = {
     systemPackages = with pkgs; [
