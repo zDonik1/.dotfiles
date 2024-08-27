9ca362857627f43da4f1471f6a520aadc74218d9
Doniyor Tokhirov <doniyor@tokhirov.uz>
2024-08-28 04:38:55 +0500

    feat(nvim): add proper error handling of invalid lua scripts

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,12 @@
-require("remap")
-require("set")
-require("lazy_init")
+local function prequire(module_name)
+	local _, ret = xpcall(function()
+		return require(module_name)
+	end, function(err)
+		vim.notify(err, vim.log.levels.ERROR)
+	end)
+	return ret
+end
+
+prequire("remap")
+prequire("set")
+prequire("lazy_init")
