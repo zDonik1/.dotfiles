752c8bedb7976f74d780d37e34be5f654c4e49f8
Doniyor Tokhirov <doniyor@tokhirov.uz>
2024-09-07 20:31:46 +0500

    fix(nvim): update noneckpain col width to accomodate gutter

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -6,6 +6,7 @@ return {
 			vim.keymap.set("n", "<leader>jn", vim.cmd.NoNeckPain, { desc = "Center wide buffers" })
 		end,
 		opts = {
+			width = 106, -- 6 additional characters for the gutter
 			buffers = {
 				wo = {
 					fillchars = "eob: ",
