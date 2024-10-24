d202b99a5669106c1a3a725557f22804ec4b8744
Doniyor Tokhirov <doniyor@tokhirov.uz>
2024-10-24 22:25:27 +0500

    fix(nvim): disable <C-m> binding in markdown plugin

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,7 +1,54 @@
 return {
+	-- creates links with <C-m> which is undocumented, clashing with harpoon
 	{
 		"ixru/nvim-markdown",
 		ft = "markdown",
+		init = function()
+			vim.g.vim_markdown_no_default_key_mappings = true
+			vim.api.nvim_create_autocmd("BufReadPre", {
+				pattern = "*.md",
+				callback = function()
+					local maps = {
+						{ mode = "n", left = "]]", right = "<Plug>Markdown_MoveToNextHeader" },
+						{ mode = "n", left = "[[", right = "<Plug>Markdown_MoveToPreviousHeader" },
+						{
+							mode = "n",
+							left = "][",
+							right = "<Plug>Markdown_MoveToNextSiblingHeader",
+						},
+						{
+							mode = "n",
+							left = "[]",
+							right = "<Plug>Markdown_MoveToPreviousSiblingHeader",
+						},
+						{ mode = "n", left = "]c", right = "<Plug>Markdown_MoveToCurHeader" },
+						{ mode = "n", left = "]u", right = "<Plug>Markdown_MoveToParentHeader" },
+						{ mode = "n", left = "<C-c>", right = "<Plug>Markdown_Checkbox" },
+						{ mode = "n", left = "<tab>", right = "<Plug>Markdown_Fold" },
+						-- { mode = "n", left = "<cr>", right = "<Plug>Markdown_FollowLink" },
+						{ mode = "n", left = "O", right = "<Plug>Markdown_NewLineAbove" },
+						{ mode = "n", left = "o", right = "<Plug>Markdown_NewLineBelow" },
+
+						{ mode = "i", left = "<tab>", right = "<Plug>Markdown_Jump" },
+						{ mode = "i", left = "<cr>", right = "<Plug>Markdown_NewLineBelow" },
+						{
+							mode = { "i", "v" },
+							left = "<C-k>",
+							right = "<Plug>Markdown_CreateLink",
+						},
+					}
+
+					for _, map in ipairs(maps) do
+						vim.keymap.set(
+							map.mode,
+							map.left,
+							map.right,
+							{ buffer = true, remap = true }
+						)
+					end
+				end,
+			})
+		end,
 	},
 
 	{
