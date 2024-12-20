f29255d1c99a5a7d7213875cb35feeedb7f3ca13
Doniyor Tokhirov <doniyor@tokhirov.uz>
2024-12-20 19:51:07 +0500

    feat(nixos): add gnome DE module

---
+++ nixos/system/gnome/default.nix
@@ -0,0 +1,23 @@
+{ ... }:
+
+{
+  services.xserver = {
+    enable = true;
+    desktopManager.gnome.enable = true;
+    displayManager.gdm.enable = true;
+
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+  };
+
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = "zdonik";
+  };
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+}
