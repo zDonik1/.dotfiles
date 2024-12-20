20fe2a475b2b737d5ef849aacbd53f34d14eb5fe
Doniyor Tokhirov <doniyor@tokhirov.uz>
2024-12-20 19:53:55 +0500

    feat(nixos): add alternate home without hyprland

---
+++ nixos/home-gui.nix
@@ -0,0 +1,50 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ./home.nix
+  ];
+
+  home.packages = with pkgs; [
+    telegram-desktop
+    keepassxc
+    vlc
+    obsidian
+    gimp
+    inkscape
+    mpv
+  ];
+
+  programs = {
+    obs-studio.enable = true;
+
+    thunderbird = {
+      enable = true;
+      profiles.zdonik.isDefault = true;
+    };
+  };
+
+  services = {
+    syncthing.enable = true;
+  };
+
+  fonts.fontconfig.enable = true;
+
+  qt = {
+    enable = true;
+    platformTheme.name = "adwaita";
+    style = {
+      package = pkgs.adwaita-qt;
+      name = "adwaita-dark";
+    };
+  };
+
+  gtk = {
+    enable = true;
+    # font.name = "";
+    theme = {
+      package = pkgs.gnome-themes-extra;
+      name = "Adwaita-dark";
+    };
+  };
+}
