9027df87fb9667c9f7b03e4e5a77b26c197811bd
Doniyor Tokhirov <doniyor@tokhirov.uz>
2024-12-21 21:12:31 +0500

    refactor(nixos): derive home-hyprland from home-gui

--- nixos/home-hyprland.nix
+++ nixos/home-hyprland.nix
@@ -1,55 +1,16 @@
 { pkgs, ... }:
 {
   imports = [
-    ./home.nix
+    ./home-gui.nix
     ./gui/hyprland
     ./gui/mako
     ./gui/keepmenu
   ];
 
   home.packages = with pkgs; [
-    telegram-desktop
-    keepassxc
     loupe
     nautilus
-    vlc
     libreoffice-qt6-fresh
     vistafonts
-    obsidian
-    gimp
-    inkscape
   ];
-
-  programs = {
-    obs-studio.enable = true;
-
-    thunderbird = {
-      enable = true;
-      profiles.zdonik.isDefault = true;
-    };
-  };
-
-  services = {
-    syncthing.enable = true;
-  };
-
-  fonts.fontconfig.enable = true;
-
-  qt = {
-    enable = true;
-    platformTheme.name = "adwaita";
-    style = {
-      package = pkgs.adwaita-qt;
-      name = "adwaita-dark";
-    };
-  };
-
-  gtk = {
-    enable = true;
-    # font.name = "";
-    theme = {
-      package = pkgs.gnome-themes-extra;
-      name = "Adwaita-dark";
-    };
-  };
 }
