a47a1d13de1b10aac8e09724a36f0a4c1ef6df64
Doniyor Tokhirov <doniyor@tokhirov.uz>
2025-01-04 08:29:27 +0500

    refactor(nixos): remove home-gui.nix

--- nixos/profiles/hyprland.nix
+++ nixos/profiles/hyprland.nix
@@ -3,18 +3,58 @@
     { pkgs, ... }:
     {
       imports = [
-        ../home-gui.nix
+        ../home/home-manager
+        ../home/cli
         ../gui/hyprland
         ../gui/mako
         ../gui/keepmenu
+        ../gui/firefox
       ];
 
       home.packages = with pkgs; [
+        telegram-desktop
+        keepassxc
+        vlc
+        obsidian
+        gimp
+        inkscape
+        mpv
         loupe
         nautilus
         libreoffice-qt6-fresh
         vistafonts
       ];
+
+      programs = {
+        obs-studio.enable = true;
+
+        thunderbird = {
+          enable = true;
+          profiles.zdonik.isDefault = true;
+        };
+      };
+
+      services.syncthing.enable = true;
+
+      fonts.fontconfig.enable = true;
+
+      qt = {
+        enable = true;
+        platformTheme.name = "adwaita";
+        style = {
+          package = pkgs.adwaita-qt;
+          name = "adwaita-dark";
+        };
+      };
+
+      gtk = {
+        enable = true;
+        # font.name = "";
+        theme = {
+          package = pkgs.gnome-themes-extra;
+          name = "Adwaita-dark";
+        };
+      };
     };
 
   system =
