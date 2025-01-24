d8b22bec5ec922db98e9cba919171e3a9fa91013
Doniyor Tokhirov <doniyor@tokhirov.uz>
2025-01-24 16:40:10 +0500

    refactor(nixos): move gnome to wm

--- nixos/system/wm/gnome/default.nix
+++ nixos/system/wm/gnome/default.nix
@@ -13,11 +13,11 @@
   };
 
   services.displayManager.autoLogin = {
-    enable = true;
+    enable = false;
     user = "zdonik";
   };
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
+  # systemd.services."getty@tty1".enable = false;
+  # systemd.services."autovt@tty1".enable = false;
 }
