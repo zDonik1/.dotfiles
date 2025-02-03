d7776bc7eca0919f179cfd72bc18ffa7e6f5c8ce
Doniyor Tokhirov <doniyor@tokhirov.uz>
2025-02-04 00:15:45 +0500

    fix(nvim): use posix shell for building telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release; cmake --build build",
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build",
 		},
 	},
 	config = function()
