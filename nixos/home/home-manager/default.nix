f34e33c79a1953e1138334b6626744ac1f461182
Doniyor Tokhirov <doniyor@tokhirov.uz>
2025-02-04 20:14:01 +0500

    feat(nixos): manage bash and zsh in hm

--- nixos/home/home-manager/default.nix
+++ nixos/home/home-manager/default.nix
@@ -1,16 +1,11 @@
-{ pkgs, config, ... }:
-let
-  # set the env vars since the default shell is nushell
-  _ = pkgs.runCommand "set-env-vars" { } ''
-    source $env.HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
-  '';
-in
+{ ... }:
+
 {
   home = {
-    sessionVariables = {
-      XDG_CONFIG_HOME = config.home.homeDirectory + "/.config";
-      XDG_DATA_HOME = config.home.homeDirectory + "/.local/share";
-    };
+    # sessionVariables = {
+    #   XDG_CONFIG_HOME = config.home.homeDirectory + "/.config";
+    #   XDG_DATA_HOME = config.home.homeDirectory + "/.local/share";
+    # };
 
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release
@@ -21,6 +16,13 @@ in
     # release notes.
     stateVersion = "24.05"; # Please read the comment before changing.
   };
+
+  # set XDG_*_HOME environment variables
+  xdg.enable = true;
+
+  # add home manager session variables to shells
+  programs.bash.enable = true;
+  programs.zsh.enable = true;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
