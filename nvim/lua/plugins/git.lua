2c646b17dea5d5f801ab281c393783203efe81c7
Doniyor Tokhirov <doniyor@tokhirov.uz>
2025-02-26 20:25:40 +0500

    feat(nvim): remove unnecessary git plugins

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,26 +1,4 @@
 return {
-	-- doesn't work with nushell for now: https://github.com/rbong/vim-flog/issues/121
-	{
-		"rbong/vim-flog",
-		lazy = true,
-		cmd = { "Flog", "Flogsplit", "Floggit" },
-		dependencies = { "tpope/vim-fugitive" },
-		init = function()
-			-- vim.keymap.set("n", "<leader>gs", vim.cmd.Floggit)
-			vim.keymap.set("n", "<leader>gl", vim.cmd.Flog)
-			vim.keymap.set("n", "<leader>gp", "<cmd>Floggit push<cr>")
-			vim.keymap.set("n", "<leader>gP", "<cmd>Floggit pull --rebase<cr>")
-			vim.keymap.set("n", "<leader>gi", function()
-				vim.fn.feedkeys(":Floggit ")
-			end)
-		end,
-	},
-
-	{
-		"tpope/vim-fugitive",
-		cmd = { "G", "Git" },
-	},
-
 	{
 		"lewis6991/gitsigns.nvim",
 		event = "VeryLazy",
@@ -60,8 +38,4 @@ return {
 			},
 		},
 	},
-
-	-- {
-	-- 	"ThePrimeagen/git-worktree.nvim",
-	-- },
 }
