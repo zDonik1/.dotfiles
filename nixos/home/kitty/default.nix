c437d55036d7c1727c2b52b44dcafd52c8de00f9
Doniyor Tokhirov <doniyor@tokhirov.uz>
2025-03-20 02:24:34 +0500

    feat(nixos): remove titlebar and fix alt in kitty on macos

--- nixos/home/kitty/default.nix
+++ nixos/home/kitty/default.nix
@@ -15,6 +15,8 @@
       bold_italic_font = "family=\"JetBrainsMono Nerd Font\" style=\"Bold Italic\"";
 
       window_padding_width = "6 8";
+      hide_window_decorations = "titlebar-only";
+      macos_option_as_alt = "yes";
 
       # make sure that nushell is started with all global/hm-session env vars
       shell = "$SHELL -c nu";
