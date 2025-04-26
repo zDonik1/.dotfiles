44dba5c57b4e874125c75b7d53fe20da1b7bde94
Doniyor Tokhirov <doniyor@tokhirov.uz>
2025-04-26 16:05:17 +0500

    feat(nixos): update how taskwarrior time reports are displayed

--- nixos/home/cli/timewarrior/default.nix
+++ nixos/home/cli/timewarrior/default.nix
@@ -4,8 +4,20 @@
   home.packages = with pkgs; [ timewarrior ];
 
   xdg.configFile = {
-    "timewarrior/timewarrior.cfg".text =
-      "import ${config.xdg.configHome}/timewarrior/themes/catppuccin.theme";
+    "timewarrior/timewarrior.cfg".text = ''
+      import ${config.xdg.configHome}/timewarrior/themes/catppuccin.theme
+
+      reports.month.spacing = 0
+      reports.week.spacing = 0
+      reports.day.spacing = 0
+
+      reports.month.totals = off
+      reports.week.totals = off
+
+      reports.month.summary = off
+      reports.week.summary = off
+    '';
+
     "timewarrior/themes/catppuccin.theme".text = ''
       define theme:
         description = "catppuccin.theme: A soothing theme for timewarrior"
