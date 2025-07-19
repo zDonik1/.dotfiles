4b385890e1c3dc023b7506d2dfaf01c96100e0ab
Doniyor Tokhirov <doniyor@tokhirov.uz>
2025-07-19 22:37:52 +0500

    feat(borgmatic): backup time and task database

--- nixos/home/cli/borgmatic/default.nix
+++ nixos/home/cli/borgmatic/default.nix
@@ -7,6 +7,13 @@ let
     cat ~/keepass/master \
       | ${pkgs.keepassxc}/bin/keepassxc-cli show -sq -a password ~/keepass/Passwords.kdbx $1
   '';
+
+  retention = {
+    keepDaily = 5;
+    keepWeekly = 3;
+    keepMonthly = 2;
+    keepYearly = 1;
+  };
 in
 {
   programs.borgmatic = {
@@ -17,26 +24,27 @@ in
           sourceDirectories = [ "${homeDir}/SecondBrain" ];
           repositories = [ "${backupsDir}/SecondBrain" ];
         };
-        retention = {
-          keepDaily = 5;
-          keepWeekly = 3;
-          keepMonthly = 2;
-          keepYearly = 1;
-        };
         storage.encryptionPasscommand = "${getKeepassEntry}/bin/get-keepass-entry SecondBrain";
+        inherit retention;
       };
       ledger = {
         location = {
           sourceDirectories = [ "${homeDir}/ledger" ];
           repositories = [ "${backupsDir}/ledger" ];
         };
-        retention = {
-          keepDaily = 5;
-          keepWeekly = 3;
-          keepMonthly = 2;
-          keepYearly = 1;
-        };
         storage.encryptionPasscommand = "${getKeepassEntry}/bin/get-keepass-entry ledger";
+        inherit retention;
+      };
+      timetask = {
+        location = {
+          sourceDirectories = [
+            "${homeDir}/.local/share/task"
+            "${homeDir}/.local/share/timewarrior"
+          ];
+          repositories = [ "${backupsDir}/timetask" ];
+        };
+        storage.encryptionPasscommand = "${getKeepassEntry}/bin/get-keepass-entry timetask";
+        inherit retention;
       };
     };
   };
