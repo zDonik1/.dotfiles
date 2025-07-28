1e650e20c16d02aced8a70f49fd39fb68161102e
Doniyor Tokhirov <doniyor@tokhirov.uz>
2025-07-28 06:32:01 +0500

    feat(nvim): add yazi.nvim plugin

---
+++ nvim/lua/plugins/yazi.lua
@@ -0,0 +1,39 @@
+return {
+	"mikavilpas/yazi.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim", lazy = true },
+	},
+	keys = {
+		-- 👇 in this section, choose your own keymappings!
+		{
+			"<leader>fc",
+			mode = { "n", "v" },
+			"<cmd>Yazi<cr>",
+			desc = "Open yazi at the current file",
+		},
+		{
+			-- Open in the current working directory
+			"<leader>fd",
+			"<cmd>Yazi cwd<cr>",
+			desc = "Open the file manager in nvim's working directory",
+		},
+		{
+			"<leader>ff",
+			"<cmd>Yazi toggle<cr>",
+			desc = "Resume the last yazi session",
+		},
+	},
+	opts = {
+		open_for_directories = false,
+		keymaps = {
+			show_help = "<f1>",
+		},
+	},
+	-- 👇 if you use `open_for_directories=true`, this is recommended
+	init = function()
+		-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+		-- vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+	end,
+}
