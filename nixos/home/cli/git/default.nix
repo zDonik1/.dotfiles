f218b32efad45180d755935824b6639eab77d58f
Doniyor Tokhirov <doniyor@tokhirov.uz>
2025-08-05 14:57:30 +0500

    feat(jujutsu): use nvim as merge tool

--- nixos/home/cli/git/default.nix
+++ nixos/home/cli/git/default.nix
@@ -8,6 +8,7 @@ in
     git-machete
     gitui
     gh-dash
+    neovim
   ];
 
   programs.git = {
@@ -35,12 +36,39 @@ in
     };
   };
 
-  programs.jujutsu = {
-    enable = true;
-    settings = {
-      user = { inherit email name; };
+  programs.jujutsu =
+    let
+      nvimdiff = pkgs.writeShellScriptBin "nvimdiff" ''
+        left=$1
+        right=$2
+        rm -f "$right/JJ-INSTRUCTIONS"
+        git -C "$left" init -q
+        git -C "$left" add -A
+        git -C "$left" commit -q -m baseline --allow-empty # create parent commit
+        mv "$left/.git" "$right"
+        git -C "$right" add --intent-to-add -A # create current working copy
+        (cd "$right"; nvim +DiffviewOpen)
+        git -C "$right" diff-index --quiet --cached HEAD && { echo "No changes done, aborting split."; exit 1; }
+        git -C "$right" commit -q -m split # create commit on top of parent including changes
+        git -C "$right" restore . # undo changes in modified files
+        git -C "$right" reset .   # undo --intent-to-add
+        git -C "$right" clean -q -df # remove untracked files
+      '';
+    in
+    {
+      enable = true;
+      settings = {
+        user = { inherit email name; };
 
-      ui.default-command = "log";
+        ui = {
+          editor = "nvim";
+          default-command = "log";
+          diff-editor = "nvim";
+        };
+
+        merge-tools.nvim = {
+          program = "${nvimdiff}/bin/nvimdiff";
+        };
+      };
     };
-  };
 }
