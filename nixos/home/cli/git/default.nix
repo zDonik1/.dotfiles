c42688bc52167d0d4a5e7e00898dbd8b1ced818f
Doniyor Tokhirov <doniyor@tokhirov.uz>
2025-08-11 15:29:27 +0500

    feat(jujustu): configure nvim as merge tool

--- nixos/home/cli/git/default.nix
+++ nixos/home/cli/git/default.nix
@@ -50,49 +50,47 @@ in
     };
   };
 
-  programs.jujutsu =
-    let
-      nvimdiff = pkgs.writeShellScriptBin "nvimdiff" ''
-        left=$1
-        right=$2
-        rm -f "$right/JJ-INSTRUCTIONS"
-        git -C "$left" init -q
-        git -C "$left" add -A
-        git -C "$left" commit -q -m baseline --allow-empty # create parent commit
-        mv "$left/.git" "$right"
-        git -C "$right" add --intent-to-add -A # create current working copy
-        (cd "$right"; nvim +DiffviewOpen)
-        git -C "$right" diff-index --quiet --cached HEAD && { echo "No changes done, aborting split."; exit 1; }
-        git -C "$right" commit -q -m split # create commit on top of parent including changes
-        git -C "$right" restore . # undo changes in modified files
-        git -C "$right" reset .   # undo --intent-to-add
-        git -C "$right" clean -q -df # remove untracked files
-      '';
-    in
-    {
-      enable = true;
-      settings = {
-        user = { inherit email name; };
+  programs.jujutsu = {
+    enable = true;
+    settings = {
+      user = { inherit email name; };
 
-        ui = {
-          editor = "nvim";
-          default-command = "log";
-          pager = "delta";
-          diff-formatter = ":git";
-          diff-editor = "nvim";
+      ui = {
+        editor = "nvim";
+        default-command = "log";
+        pager = "delta";
+        diff-formatter = ":git";
+        diff-editor = "nvim";
+        merge-editor = "nvim";
+      };
+
+      merge-tools = {
+        delta = {
+          diff-expected-exit-codes = [
+            0
+            1
+          ];
         };
 
-        merge-tools = {
-          nvim = {
-            program = "${nvimdiff}/bin/nvimdiff";
-          };
-          delta = {
-            diff-expected-exit-codes = [
-              0
-              1
-            ];
-          };
+        nvim = {
+          program = "nvim";
+          edit-args = [
+            "-c"
+            "DiffEditor $left $right $output"
+          ];
+          merge-args = [
+            "-c"
+            "let g:jj_diffconflicts_marker_length=$marker_length"
+            "-c"
+            "JJDiffConflicts!"
+            "$output"
+            "$base"
+            "$left"
+            "$right"
+          ];
+          merge-tool-edits-conflict-markers = true;
         };
       };
     };
+  };
 }
