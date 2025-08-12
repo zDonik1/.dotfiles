354af22d4be856ef703be4515cccc6d5ea757e06
Doniyor Tokhirov <doniyor@tokhirov.uz>
2025-08-13 00:11:09 +0500

    refactor(nvim): put mappings in key in vcsigns config

--- nvim/lua/plugins/vcs.lua
+++ nvim/lua/plugins/vcs.lua
@@ -7,45 +7,67 @@ return {
 	{
 		"algmyr/vcsigns.nvim",
 		event = "VeryLazy",
+		keys = {
+			{
+				"[r",
+				function()
+					require("vcsigns").actions.target_older_commit(0, vim.v.count1)
+				end,
+				desc = "Move diff target back",
+			},
+			{
+				"]r",
+				function()
+					require("vcsigns").actions.target_newer_commit(0, vim.v.count1)
+				end,
+				desc = "Move diff target forward",
+			},
+			{
+				"[h",
+				function()
+					require("vcsigns").actions.hunk_prev(0, vim.v.count1)
+				end,
+				desc = "Go to previous hunk",
+			},
+			{
+				"]h",
+				function()
+					require("vcsigns").actions.hunk_next(0, vim.v.count1)
+				end,
+				desc = "Go to next hunk",
+			},
+			{
+				"[H",
+				function()
+					require("vcsigns").actions.hunk_prev(0, 9999)
+				end,
+				desc = "Go to first hunk",
+			},
+			{
+				"]H",
+				function()
+					require("vcsigns").actions.hunk_next(0, 9999)
+				end,
+				desc = "Go to last hunk",
+			},
+			{
+				"<leader>gr",
+				function()
+					require("vcsigns").actions.hunk_undo(0)
+				end,
+				desc = "Undo hunks in range",
+			},
+			{
+				"<leader>gh",
+				function()
+					require("vcsigns").actions.toggle_hunk_diff(0)
+				end,
+				desc = "Show hunk diffs inline in the current buffer",
+			},
+		},
 		opts = {
 			target_commit = 1, -- Nice default for jj with new+squash flow.
 		},
-		config = function(_, opts)
-			require("vcsigns").setup(opts)
-
-			local function map(mode, lhs, rhs, desc, opt)
-				local options = { noremap = true, silent = true, desc = desc }
-				if opt then
-					options = vim.tbl_extend("force", options, opt)
-				end
-				vim.keymap.set(mode, lhs, rhs, options)
-			end
-
-			map("n", "[r", function()
-				require("vcsigns").actions.target_older_commit(0, vim.v.count1)
-			end, "Move diff target back")
-			map("n", "]r", function()
-				require("vcsigns").actions.target_newer_commit(0, vim.v.count1)
-			end, "Move diff target forward")
-			map("n", "[h", function()
-				require("vcsigns").actions.hunk_prev(0, vim.v.count1)
-			end, "Go to previous hunk")
-			map("n", "]h", function()
-				require("vcsigns").actions.hunk_next(0, vim.v.count1)
-			end, "Go to next hunk")
-			map("n", "[H", function()
-				require("vcsigns").actions.hunk_prev(0, 9999)
-			end, "Go to first hunk")
-			map("n", "]H", function()
-				require("vcsigns").actions.hunk_next(0, 9999)
-			end, "Go to last hunk")
-			map("n", "<leader>gr", function()
-				require("vcsigns").actions.hunk_undo(0)
-			end, "Undo hunks in range")
-			map("n", "<leader>gh", function()
-				require("vcsigns").actions.toggle_hunk_diff(0)
-			end, "Show hunk diffs inline in the current buffer")
-		end,
 	},
 
 	{
