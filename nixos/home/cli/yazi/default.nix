f8ae73a8a634ba74ace69899047bef657827caba
Doniyor Tokhirov <doniyor@tokhirov.uz>
2025-09-24 21:52:16 +0500

    feat(yazi): add more keybinds for half page navigation

--- nixos/home/cli/yazi/default.nix
+++ nixos/home/cli/yazi/default.nix
@@ -1,22 +1,22 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    ffmpegthumbnailer
-    p7zip
-    jq
-    poppler
-    fd
-    ripgrep
-    fzf
-    zoxide
-    imagemagick
-    exiftool
-    mpv
-  ];
-
   programs.yazi = {
     enable = true;
+    extraPackages = with pkgs; [
+      ffmpegthumbnailer
+      p7zip
+      jq
+      poppler
+      fd
+      ripgrep
+      fzf
+      zoxide
+      imagemagick
+      exiftool
+      mpv
+    ];
+
     initLua = ./init.lua;
     shellWrapperName = "y";
 
@@ -26,6 +26,26 @@
         show_hidden = true;
         scrolloff = 5;
       };
+    };
+
+    keymap = {
+      mgr.prepend_keymap = [
+        {
+          on = "<C-s>";
+          run = "arrow -50%";
+          desc = "Move cursor up half page";
+        }
+        {
+          on = "<C-t>";
+          run = "arrow 50%";
+          desc = "Move cursor down half page";
+        }
+        {
+          on = "<C-x>";
+          run = "escape --search";
+          desc = "Cancel the ongoing search";
+        }
+      ];
     };
   };
 
