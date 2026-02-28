86c43faf719c798b36e6176052b0dc70821b7256
Doniyor Tokhirov <doniyor@tokhirov.uz>
2026-02-28 06:55:58 +0500

    fix(nvim): update treesitter deprecated function usage

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,79 +1,118 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-context",
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		build = function()
+			vim.cmd("TSUpdate")
+		end,
+
+		keys = function()
+			local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+			return {
+				{
+					";",
+					ts_repeat_move.repeat_last_move,
+					desc = "TS repeat last move",
+					mode = { "n", "x", "o" },
+				},
+				{
+					",",
+					ts_repeat_move.repeat_last_move_opposite,
+					desc = "TS repeat last move opposite",
+					mode = { "n", "x", "o" },
+				},
+				{
+					"f",
+					ts_repeat_move.builtin_f_expr,
+					expr = true,
+					desc = "Go to letter",
+					mode = { "n", "x", "o" },
+				},
+				{
+					"F",
+					ts_repeat_move.builtin_F_expr,
+					expr = true,
+					desc = "Go to letter backwards",
+					mode = { "n", "x", "o" },
+				},
+				{
+					"t",
+					ts_repeat_move.builtin_t_expr,
+					expr = true,
+					desc = "Go to letter not reaching",
+					mode = { "n", "x", "o" },
+				},
+				{
+					"T",
+					ts_repeat_move.builtin_T_expr,
+					expr = true,
+					desc = "Go to letter not reaching backwards",
+					mode = { "n", "x", "o" },
+				},
+			}
+		end,
+
+		opts = {
+			auto_install = true,
+			ensure_installed = { "lua" },
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+				disable = { "csv" },
+			},
+			indent = { enable = true },
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+						["as"] = "@scope.outer",
+						["ar"] = "@parameter.outer",
+						["ir"] = "@parameter.inner",
+					},
+					selection_modes = {
+						["@function.outer"] = "V",
+						["@class.outer"] = "V",
+					},
+					include_surrounding_whitespace = true,
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]f"] = "@function.outer",
+						["]c"] = "@class.outer",
+					},
+					goto_next_end = {
+						["]F"] = "@function.outer",
+						["]C"] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[f"] = "@function.outer",
+						["[c"] = "@class.outer",
+					},
+					goto_previous_end = {
+						["[F"] = "@function.outer",
+						["[C"] = "@class.outer",
+					},
+				},
+			},
+		},
+	},
+
+	{
 		"nvim-treesitter/nvim-treesitter-context",
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		"lewis6991/gitsigns.nvim",
-	},
-	build = function()
-		vim.cmd("TSUpdate")
-	end,
-	opts = {
-		auto_install = true,
-		ensure_installed = { "lua" },
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-			disable = { "csv" },
-		},
-		indent = { enable = true },
-		textobjects = {
-			select = {
-				enable = true,
-				lookahead = true,
-				keymaps = {
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@class.outer",
-					["ic"] = "@class.inner",
-					["as"] = "@scope.outer",
-					["ar"] = "@parameter.outer",
-					["ir"] = "@parameter.inner",
-				},
-				selection_modes = {
-					["@function.outer"] = "V",
-					["@class.outer"] = "V",
-				},
-				include_surrounding_whitespace = true,
-			},
-			move = {
-				enable = true,
-				set_jumps = true,
-				goto_next_start = {
-					["]f"] = "@function.outer",
-					["]]"] = "@class.outer",
-				},
-				goto_next_end = {
-					["]F"] = "@function.outer",
-					["]["] = "@class.outer",
-				},
-				goto_previous_start = {
-					["[f"] = "@function.outer",
-					["[["] = "@class.outer",
-				},
-				goto_previous_end = {
-					["[F"] = "@function.outer",
-					["[]"] = "@class.outer",
-				},
-			},
+		optional = true,
+		opts = {
+			separator = "─",
+			max_lines = 5,
 		},
 	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-		require("treesitter-context").setup({ separator = "─", max_lines = 5 })
-
-		local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-		vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-		vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
-		vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-		vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-		vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-		vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
-
-		local gs = require("gitsigns")
-		local next_hunk_repeat, prev_hunk_repeat =
-			ts_repeat_move.make_repeatable_move_pair(gs.next_hunk, gs.prev_hunk)
-		vim.keymap.set({ "n", "x", "o" }, "]h", next_hunk_repeat)
-		vim.keymap.set({ "n", "x", "o" }, "[h", prev_hunk_repeat)
-	end,
 }
