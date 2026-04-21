e6471426aeb310de39261265642b48c38a72702c
Doniyor Tokhirov <doniyor@tokhirov.uz>
2026-04-21 10:01:58 +0500

    fix(nvim): fix all deprecations

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -28,10 +28,12 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
 		if client:supports_method("textDocument/codeLens") then
-			vim.lsp.codelens.refresh()
+			vim.lsp.codelens.enable(true, { bufnr = bufnr })
 			vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
 				buffer = bufnr,
-				callback = vim.lsp.codelens.refresh,
+				callback = function()
+					vim.lsp.codelens.enable(true, { bufnr = bufnr })
+				end,
 			})
 		end
 	end,
